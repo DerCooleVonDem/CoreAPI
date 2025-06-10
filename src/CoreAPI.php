51d914684df01a8af7f4469fd376128a050c93db
Jonas <jonaswindmann@gmail.com>
2025-06-10 11:31:26 +0200

    Change

---
+++ src/CoreAPI.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JonasWindmann\CoreAPI;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\SingletonTrait;
+
+class CoreAPI extends PluginBase{
+
+    use SingletonTrait;
+
+
+
+}
